9fe1627e838f0eac223e942f2ab8f031761a7c1f
Rafael Azambuja <rafael.estevam.93@gmail.com>
2018-08-31 10:14:20 -0300

    avaliacao s2

---
+++ Avaliacao-s2it/src/exercicio8/BuscaNumero.java
@@ -0,0 +1,12 @@
+package exercicio8;
+
+abstract class BuscaNumero {
+	
+	public char pegaNumero(String numero, int posicao) {
+	
+		char unitario = numero.charAt(posicao);
+		
+		return unitario;
+	}
+
+}
---
+++ Avaliacao-s2it/src/exercicio8/Resolucao8.java
@@ -0,0 +1,22 @@
+package exercicio8;
+
+
+public class Resolucao8 extends BuscaNumero {
+	
+			public static void main(String[] args)
+			{
+				String a = "10256";
+				String b = "512";
+
+				Resolucao8 numeroC = new Resolucao8();
+				
+			char c1 = numeroC.pegaNumero(a, 0);
+			char c2 = numeroC.pegaNumero(b, 0);
+			char c3 = numeroC.pegaNumero(a, 1);
+			char c4 = numeroC.pegaNumero(b, 1);
+			
+				
+			}
+			
+			
+		}
